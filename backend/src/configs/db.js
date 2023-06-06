5f735d4b7226d2adae74730cc934a44b744d8722
surajmaurya0 <135691193+surajmaurya0@users.noreply.github.com>
2023-06-06 15:07:05 +0530

    change mongodb

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/crud')
+mongoose.connect('mongodb+srv://surajdevv1:Tv6Sov2ySxskNnyo@cluster0.xdfnvfy.mongodb.net/')
