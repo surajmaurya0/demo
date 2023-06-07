9d7eba756428b9af368ff55e33efab39261be9c0
surajmaurya0 <135691193+surajmaurya0@users.noreply.github.com>
2023-06-07 12:30:09 +0530

    "data base"

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://surajdevv1:Tv6Sov2ySxskNnyo@cluster0.xdfnvfy.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  retryWrites: true, 
 })
   .then(() => {
     console.log('Connected to MongoDB');
