1465387b752813ff38c9da669b4098fc6efebbc8
surajmaurya0 <135691193+surajmaurya0@users.noreply.github.com>
2023-06-07 12:15:31 +0530

    "data base"

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,3 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://surajdevv1:Tv6Sov2ySxskNnyo@cluster0.xdfnvfy.mongodb.net/')
+mongoose.connect('mongodb+srv://surajdevv1:Tv6Sov2ySxskNnyo@cluster0.xdfnvfy.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start your application or perform database operations
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
